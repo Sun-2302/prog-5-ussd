3d05c9ddc4e7b7a2db8cf8afbb064db5dee4b161
Sun-2302 <hei.dera.932@gmail.com>
2025-05-16 17:44:12 +0300

    ci: add GitHub Actions lint job

--- lint.sh
+++ lint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-java -jar checkstyle.jar -c checkstyle.xml src/
+
+JAR=checkstyle.jar
+URL=https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.12.3/checkstyle-10.12.3-all.jar
+
+if [ ! -f "$JAR" ]; then
+  echo "Téléchargement de $JAR..."
+  curl -L "$URL" -o "$JAR"
+fi
+
+java -jar "$JAR" -c checkstyle.xml src/
+
+
 
